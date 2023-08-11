6809f2cac1d7650df205391aae01bd0529074f69
Emauel0124 <emanuelbedoya0124@gmail.com>
2023-08-11 09:11:21 -0500

    Se hace eñ punto 2 y el 3

--- App.py
+++ App.py
@@ -1,3 +1,9 @@
 from Punto1_carro import carro
+from Punto2_y_3_plano import punto
+
+
 
 Vehiculo_1=carro("200", "1500")
+Punto_plano=punto(2,8)
+Punto_plano.mover()
+
---
+++ Punto2_y_3_plano.py
@@ -0,0 +1,15 @@
+class punto:
+    def __init__(self, Coordenada_X, Coordenada_Y) -> None:
+        
+        self.Coordenada_X=Coordenada_X
+        self.Coordenada_Y=Coordenada_Y
+
+        print(f"El punto en el plano cartesiano tiene las coordenadas ({Coordenada_X},{Coordenada_Y})")
+
+    def mover(self):
+        mover_puntoX=int(input("Digite las coordenadas que desea para el punto en el eje X:  "))
+        mover_puntoY=int(input("Digite las coordenadas que desea para el punto en el eje Y:  "))
+        print(f"Las nuevas coordenadas del punto son: ({mover_puntoX},{mover_puntoY})")
+
+
+    
